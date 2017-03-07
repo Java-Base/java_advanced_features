4658a0f0fc77900005637519904623acf81172e8
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2017-03-07 19:02:31 +0800

    add files

---
+++ java_concurrent/src/main/java/com/java/advanced/features/concurrent/Test.java
@@ -0,0 +1,11 @@
+package com.java.advanced.features.concurrent;
+
+/**
+ * $
+ *
+ * @author Lian
+ * @date 2017/3/3
+ * @since 1.0
+ */
+public class Test {
+}
--- java_io/src/main/java/com/java/advanced/features/io/util/PathUtil.java
+++ java_io/src/main/java/com/java/advanced/features/io/util/PathUtil.java
@@ -25,7 +25,7 @@ public class PathUtil {
 	}
 
 	/**
-	 * 
+	 * 获取规范路径
 	 * 
 	 * @return
 	 * @throws IOException 
@@ -35,7 +35,7 @@ public class PathUtil {
 	}
 
 	/**
-	 * 
+	 * 获取绝对路径
 	 * 
 	 * @return
 	 */
